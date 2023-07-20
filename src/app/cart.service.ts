d06681c182bd990036938e640c6c5a69383d4533
Tim Holmes <tim@gravitaz.co.uk>
2023-07-20 10:02:37 +0100

    Shipping

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 import { Injectable } from '@angular/core';
 
@@ -20,5 +21,12 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  /* . . . */
+
+  getShippingPrices() {
+    return this.http.get<{ type: string; price: number }[]>(
+      '/assets/shipping.json'
+    );
+  }
+
+  constructor(private http: HttpClient) {}
 }
