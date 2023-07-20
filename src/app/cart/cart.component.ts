48d2a1f023e3ad7f327a7793f42b01e068c7411c
Tim Holmes <tim@gravitaz.co.uk>
2023-07-20 10:26:40 +0100

    Checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 import { CartService } from '../cart.service';
 
 @Component({
@@ -9,5 +10,22 @@ import { CartService } from '../cart.service';
 export class CartComponent {
   items = this.cartService.getItems();
 
-  constructor(private cartService: CartService) {}
+  checkoutForm = this.formBuilder.group({
+    name: 'Tim',
+    address: '',
+  });
+
+  onSubmit() {
+    window.alert(
+      `Your order has been submitted ${JSON.stringify(this.checkoutForm.value)}`
+    );
+    console.warn(this.checkoutForm.value);
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+  }
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
 }
